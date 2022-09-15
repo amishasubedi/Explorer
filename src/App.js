db5e088eeacf76ac76e3e6312163e3eb19fadd02
amishasubedi <amisha.subedi1@gmail.com>
2022-09-15 00:10:48 -0500

    initialized, setup pages, and routes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlaces from "./places/components/pages/NewPlaces";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+
+        <Route path="/places/new" exact>
+          <NewPlaces />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
