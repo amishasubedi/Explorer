a3f9321ce2dba345a5b26add0f6b9e637119408e
amishasubedi <amisha.subedi1@gmail.com>
2022-09-15 00:47:35 -0500

    worked with user components and pages

---
+++ src/shared/components/UI/Design.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Design.css";
+
+const Design = (props) => {
+  return (
+    <div className={`avatar ${props.className}`} style={props.style}>
+      <img
+        src={props.image}
+        alt={props.alt}
+        style={{ width: props.width, height: props.width }}
+      />
+    </div>
+  );
+};
+
+export default Design;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>USer page works</h2>;
+  const dummyData = [
+    {
+      id: 1,
+      name: "Amisha",
+      image:
+        "https://media-exp1.licdn.com/dms/image/C5603AQFHoqZRoC8JWQ/profile-displayphoto-shrink_200_200/0/1654656935350?e=1668643200&v=beta&t=IUWljsCJ_jpGmY2gHYP6D4__3dq3j3N0hB1NZP8hjIQ",
+      placeCount: 3,
+    },
+  ];
+  return <UsersList items={dummyData} />;
 };
 
 export default Users;
