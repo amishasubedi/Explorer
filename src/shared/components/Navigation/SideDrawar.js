e5964602aa35ee29eba9e42bb1e65fbe6abd07dd
amishasubedi <amisha.subedi1@gmail.com>
2022-09-15 22:03:38 -0500

    rendered side drawar outside of root

--- src/shared/components/Navigation/SideDrawar.js
+++ src/shared/components/Navigation/SideDrawar.js
@@ -1,9 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import "./SideDrawer.css";
 
 const SideDrawar = (props) => {
-  return <aside className="side-drawar">{props.children}</aside>;
+  const content = <aside className="side-drawar">{props.children}</aside>;
+
+  //  render side drawar in drawer hook (outside of root)
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
 };
 
 export default SideDrawar;
