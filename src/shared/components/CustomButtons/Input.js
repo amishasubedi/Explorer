74e194f8f1ae4fc4744505d3d98719772bafdb1d
asubedi <asubedi@go.olemiss.edu>
2022-10-06 23:57:27 -0500

    multiple inputs and form validity

--- src/shared/components/CustomButtons/Input.js
+++ src/shared/components/CustomButtons/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { validate } from "../../util/validators";
 
 import "./Input.css";
@@ -28,6 +28,14 @@ const Input = (props) => {
     isTouched: false,
     isValid: false,
   });
+
+  // extract required items through object destructuring
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    props.onInput(props.id, inputState.value, inputState.valid);
+  }, [id, onInput, value, isValid]);
 
   const changeHandler = (event) => {
     dispatch({
